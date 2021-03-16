43dea5b9f38e9f3102ef5ec463c7b393be84e29b
rudgjs8080 <kkh_8080@naver.com>
2021-03-16 16:42:04 +0900

    연산

---
+++ Java_00_Hello/src/com/callor/hello/Nums02.java
@@ -0,0 +1,15 @@
+package com.callor.hello;
+
+public class Nums02 {
+	
+	public static void main(String[] args) {
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30000 / 300);
+		// % >> 나머지를 구하는 연산자
+		
+
+	}
+
+}
