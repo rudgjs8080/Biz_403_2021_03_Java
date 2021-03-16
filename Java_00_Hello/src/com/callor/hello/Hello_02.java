1e8dee5ad87f297559763325bbdc039d82556611
rudgjs8080 <kkh_8080@naver.com>
2021-03-16 14:58:53 +0900

    Hello_02 추가

--- Java_00_Hello/src/com/callor/hello/Hello_02.java
+++ Java_00_Hello/src/com/callor/hello/Hello_02.java
@@ -3,7 +3,8 @@ package com.callor.hello;
 public class Hello_02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("반갑습니다");
+		System.out.println(30 * 40);
 
 	}
 
